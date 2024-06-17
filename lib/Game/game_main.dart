0e984a13f67fd5c2686253ca1e26f1387168ed98
oja-bitterlife <bitter.life.club@gmail.com>
2024-06-17 18:32:24 +0900

    プレイヤー追加開始

--- lib/Game/game_main.dart
+++ lib/Game/game_main.dart
@@ -5,6 +5,8 @@ import '../logger.dart';
 
 import '../UI_Widgets/message_window.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flame/Sprite.dart';
+import 'package:flame/components.dart';
 
 class MyGame extends FlameGame with TapCallbacks {
   late TiledMap map;
@@ -15,26 +17,27 @@ class MyGame extends FlameGame with TapCallbacks {
 
     log.d("onLoad");
 
-    // final windowImg = await Sprite.load('window.png');
-    // windowImg.image.(Rect.fromLTRB(15, 15, 35, 35))
-    // await add(
-    //   SpriteComponent(
-    //     position: Vector2(size.x * 0.5, size.y * 0.5),
-    //     anchor: Anchor.center,
-    //     sprite: windowImg,
-    //   ),
-    // );
-    // final img = Image.asset(
-    //   "images/window.png",
-    //   height: 100,
-    //   width: 350,
-    //   centerSlice: const Rect.fromLTRB(15, 15, 35, 35),
-    // );
-    // Sprite(img);
+    final playerSheet = SpriteSheet.fromColumnsAndRows(
+        image: await images.load('sample20160312.png'), columns: 18, rows: 12);
 
     final TiledComponent tiledMap =
         await TiledComponent.load("map.tmx", Vector2(32.0, 32.0));
     await add(tiledMap);
+
+    final player = SpriteAnimationComponent(
+      animation: SpriteAnimation.fromFrameData(
+          playerSheet.image,
+          SpriteAnimationData([
+            playerSheet.createFrameData(8, 6, stepTime: 0.5),
+            playerSheet.createFrameData(8, 7, stepTime: 0.5),
+            playerSheet.createFrameData(8, 8, stepTime: 0.5),
+            playerSheet.createFrameData(8, 7, stepTime: 0.5),
+          ])),
+      anchor: Anchor.bottomCenter,
+      size: Vector2(48, 64),
+    );
+    await add(player);
+    player.position = Vector2(64, 448);
 
     const msgWin = GlobalObjectKey<MessageWindowState>("MessageWindow");
     msgWin.currentState?.show("あいうえお\nかきくけ\nさしす\nたち");
