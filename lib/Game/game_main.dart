bc869ef79f22060dafac05e7cb9ebdd9d3294954
ozaki <bitter.life.club@gmail.com>
2024-06-21 02:35:49 +0900

    方向変更追加

--- lib/Game/game_main.dart
+++ lib/Game/game_main.dart
@@ -1,13 +1,16 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/events.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 
 // import '../logger.dart';
 import '../UI_Widgets/message_window.dart';
 import 'player.dart';
 import 'map.dart';
+import '../logger.dart';
 
-class MyGame extends FlameGame with TapCallbacks {
+class MyGame extends FlameGame with TapCallbacks, KeyboardEvents {
   late PlayerComponent player;
 
   @override
@@ -32,8 +35,35 @@ class MyGame extends FlameGame with TapCallbacks {
 
     const msgWin = GlobalObjectKey<MessageWindowState>("MessageWindow");
     msgWin.currentState?.hide();
+  }
 
-    player.setDir(PlayerDir.left);
+  @override
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    const msgWin = GlobalObjectKey<MessageWindowState>("MessageWindow");
+
+    // メッセージウインドウ表示中は動かさない
+    if (msgWin.currentState!.isVisible) {
+      return KeyEventResult.ignored;
+    }
+
+    // キーボードで動かす
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        player.setDir(PlayerDir.left);
+        return KeyEventResult.handled;
+      case LogicalKeyboardKey.arrowRight:
+        player.setDir(PlayerDir.right);
+        return KeyEventResult.handled;
+      case LogicalKeyboardKey.arrowUp:
+        player.setDir(PlayerDir.up);
+        return KeyEventResult.handled;
+      case LogicalKeyboardKey.arrowDown:
+        player.setDir(PlayerDir.down);
+        return KeyEventResult.handled;
+    }
+
+    return KeyEventResult.ignored;
   }
 
   // @override
