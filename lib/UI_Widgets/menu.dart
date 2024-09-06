c82fd1d95adf29d18b262f0a817ef3d200d9bd5f
oja-bitterlife <bitter.life.club@gmail.com>
2024-09-06 15:17:36 +0900

    Lv表記追加

--- lib/UI_Widgets/menu.dart
+++ lib/UI_Widgets/menu.dart
@@ -1,7 +1,6 @@
-import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import '../Game/ui_control.dart';
+import 'save_load.dart';
 
 // ignore: unused_import
 import '../my_logger.dart';
@@ -60,88 +59,10 @@ class Menu extends StatelessWidget {
               await showDialog(
                   context: context,
                   builder: (context) {
-                    return const SaveLoadDialog();
+                    return SaveLoadDialog(myGame);
                   });
             }),
       ],
     );
-  }
-}
-
-class SaveLoadDialog extends StatelessWidget {
-  const SaveLoadDialog({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const SimpleDialog(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.0))),
-      title: Text('Save/Load'),
-      alignment: Alignment.topLeft,
-      children: [
-        SimpleDialogOption(
-          child: SaveLoadCard(),
-        ),
-        SimpleDialogOption(
-          child: SaveLoadCard(),
-        ),
-        SimpleDialogOption(
-          child: SaveLoadCard(),
-        ),
-      ],
-    );
-  }
-}
-
-class SaveLoadCard extends StatelessWidget {
-  const SaveLoadCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        shadowColor: Colors.transparent,
-        shape: const RoundedRectangleBorder(
-          side: BorderSide(color: Colors.lightGreen, width: 2),
-        ),
-        color: Colors.transparent,
-        child: SizedBox(
-          height: 80,
-          child: Row(
-            children: [
-              const SizedBox(width: 80, height: 80),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 4),
-                  const Row(children: [
-                    SizedBox(width: 8),
-                    Text(
-                      "--/--/-- --:--:--",
-                      style: TextStyle(fontSize: 20),
-                    )
-                  ]),
-                  const SizedBox(height: 4),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.deepOrange[300],
-                              foregroundColor: Colors.white),
-                          onPressed: () {},
-                          child: const Text("Save")),
-                      const SizedBox(width: 4),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.lightBlue[300],
-                              foregroundColor: Colors.white),
-                          onPressed: () {},
-                          child: const Text("Load"))
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
   }
 }
--- lib/UI_Widgets/save_load.dart
+++ lib/UI_Widgets/save_load.dart
@@ -1,17 +1,92 @@
 import 'package:flutter/material.dart';
-import '../Game/ui_control.dart';
 
 // ignore: unused_import
 import '../my_logger.dart';
 import '../Game/my_game.dart';
 
-// class SaveLoad extends StatelessWidget {
-//   final MyGame myGame;
-//   const SaveLoad({super.key, required this.myGame});
+class SaveLoadDialog extends StatelessWidget {
+  final MyGame myGame;
+  const SaveLoadDialog(this.myGame, {super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Flow(
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0))),
+      title: const Text('Save/Load'),
+      alignment: Alignment.topLeft,
+      children: [
+        SimpleDialogOption(
+          child: SaveLoadCard(myGame),
+        ),
+        SimpleDialogOption(
+          child: SaveLoadCard(myGame),
+        ),
+        SimpleDialogOption(
+          child: SaveLoadCard(myGame),
+        ),
+      ],
+    );
+  }
+}
+
+class SaveLoadCard extends StatelessWidget {
+  final MyGame myGame;
+  const SaveLoadCard(this.myGame, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        shadowColor: Colors.transparent,
+        shape: const RoundedRectangleBorder(
+          side: BorderSide(color: Colors.lightGreen, width: 2),
+        ),
+        color: Colors.transparent,
+        child: SizedBox(
+          height: 80,
+          child: Row(
+            children: [
+              const SizedBox(width: 80, height: 80),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 4),
+                  const Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(width: 8),
+                        Text(
+                          "--/--/-- --:--:--",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          "Lv.0",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ]),
+                  const SizedBox(height: 4),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.deepOrange[300],
+                              foregroundColor: Colors.white),
+                          onPressed: () {},
+                          child: const Text("Save")),
+                      const SizedBox(width: 4),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.lightBlue[300],
+                              foregroundColor: Colors.white),
+                          onPressed: () {},
+                          child: const Text("Load"))
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
